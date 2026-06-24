59cd39ed2e4931445ae1d8f5a2abaf640a92fca2
TrixieMacharia <machariastacy034@gmail.com>
2026-06-24 13:12:38 +0300

    modified:   moonlight/moonlight/settings.py modified:   moonlight/requirements.txt

--- moonlight/moonlight/settings.py
+++ moonlight/moonlight/settings.py
@@ -84,7 +84,7 @@ DATABASE_URL = config('DATABASE_URL', default=None)
 if DATABASE_URL:
     #Render PostgreSQL
     DATABASES = {
-        'default'" dj_database_url.parse(DATABASE_URL)
+        'default': dj_database_url.parse(DATABASE_URL)
     }
 else:
     DATABASES = {
